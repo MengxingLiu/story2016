f792b0e1b4d6f8e279adc2c5b42e1729a9fa76b8
Max <mengluo1990@gmail.com>
2017-03-12 13:37:38 +0800

    simple correlation

---
+++ ROIana/s2_simple_Correlation_FFG.sh
@@ -0,0 +1,93 @@
+##################################################################
+# Step 1: Get the seed region at FFG: ICs showed significant interaction
+#================================================================
+#!/bin/csh
+set spath = "/public/home/max/story2016fMRI/"
+cd "$spath"/group/func_connect
+     mkdir mask
+    
+     3dcalc -prefix ./mask/FFG_lh_listen.nii.gz \
+          -a "$spath"/group/MVM/Clustermask.nii.gz\[4] \   # Listen_CS
+          -b "$spath"/group/MVM/Clustermask.nii.gz\[5] \   # Listen_US
+          -c "$spath"/group/MVM/Clustermask.nii.gz\[6] \   # Listen_SW
+          -d "$spath"/group/anat_ROIs/lh_FFG.nii.gz \      # FFG mask
+          -expr "(step(a)+step(b)+step(c))*d"
+     3dcalc -prefix ./mask/FFG_lh_read.nii.gz \
+          -a "$spath"/group/MVM/Clustermask.nii.gz\[7] \   # Read_CS
+          -b "$spath"/group/MVM/Clustermask.nii.gz\[8] \   # Read_US
+          -c "$spath"/group/MVM/Clustermask.nii.gz\[9] \   # Read_SW
+          -d "$spath"/group/anat_ROIs/lh_FFG.nii.gz \      # FFG mask
+          -expr "(step(a)+step(b)+step(c))*d"
+
+
+##################################################################
+# Step 2: Compute the simple correlation analysis for each subject
+#================================================================
+#!/bin/csh
+set spath = "/Users/jfyang/projects/task_VWFA/analysis"
+
+#=====Here get the ts for each condition
+foreach task( listen read)
+    if $task = "listen" then
+        set sublist = ( sub02 sub03 sub04 sub05 sub06 sub07 sub08 sub09 \
+                    sub12 sub13 sub15 sub16 sub31 sub32)
+    else 
+        set sublist = ( sub10 sub11 sub14 sub17 sub19 sub20 sub21 \
+                 sub22 sub23 sub24 sub25 sub26 sub27 sub28 \
+                 sub29 sub33)
+    endif
+
+    foreach sub( $sublist )
+        #mkdir "$spath"/"$sub"/simCorrAna/
+        mkdir -p "$spath"/group/func_connect/"$sub"/
+        cd "$spath"/group/func_connect/"$sub"/
+        echo "===========Simple Correlation Analysis for: $sub  ==================="
+        set tsfile = "$spath"/"$sub"/map2subAvg/ts.nii
+        3dSynthesize -cbucket "$spath"/"$sub"/map2subAvg/GLM.cbucket.nii.gz \
+            -matrix "$spath"/"$sub"/orig_files/GLM.x1D -select \
+            0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 24 25 26 27 28 29 \
+            -prefix ts_NoInterest.nii.gz
+
+        3dcalc -prefix ts_CleanData_All.nii.gz \
+               -a $tsfile \
+               -b ts_NoInterest.nii.gz \
+               -expr '(a-b)'
+          #     rm "$type"_NoInterest+tlrc.nii.gz
+        foreach type( CS US SW NS )
+                 # split the whole time series into 4 ts, for 4 conditions
+            3dTcat -prefix ts_CleanData_"$type".nii.gz \
+                ts_CleanData_All.nii.gz"[1dcat /public/home/max/scripts/story2016/index_$type.1D]"
+           # Here 3dAutomask not worked well no-skulled dataset, so we get it from Anatomical dataset
+                #3dAutomask -prefix CleanData_mask CleanData_at+tlrc
+                #3dAutomask -prefix CleanData_mask Anatomical_reg_AlndExp_ns_at+tlrc
+          # Here Our Dataset are based on skullstripped, so itself can be the mask
+               3dmaskave -quiet -mask ts_CleanData_"$type".nii.gz \
+                         ts_CleanData_"$type".nii.gz > ts_Global_"$type".1D
+
+                3dmaskave -mask "$spath"/group/func_connect/mask/FFG_lh_"$task".nii.gz \
+                          ts_CleanData_"$type".nii.gz | awk '{print $1}' \
+                          > ts_lh_FFG_"$type".1D
+
+               3dDeconvolve -input ts_CleanData_"$type".nii.gz \
+                    -polort A \
+                    -num_stimts 2 \
+                    -stim_file 1 ts_Global_"$type".1D -stim_label 1 NoInterest \
+                    -stim_file 2 ts_lh_FFG_"$type".1D -stim_label 2 lh_FFG \
+                    -tout -rout \
+                    -bucket Corr_lh_FFG_"$type".nii.gz
+                    rm *.REML_cmd
+                    rm *.xmat.1D
+                    rm *.xsave
+               3dcalc -prefix Corr_lh_FFG_"$type"_R.nii.gz \
+                         -a Corr_lh_FFG_"$type".nii.gz'[7]' \
+                         -b Corr_lh_FFG_"$type".nii.gz'[5]' \
+                         -expr "ispositive(b)*sqrt(a)-isnegative(b)*sqrt(a)"
+               3dcalc -prefix Corr_lh_FFG_"$type"_Z.nii.gz \
+                         -a Corr_lh_FFG_"$type"_R+tlrc.nii.gz \
+                         -expr 'log((1+a)/(1-a))/2'
+               rm Corr_lh_FFG_"$type"_R+tlrc.nii.gz
+          end
+     end
+end
+echo "===================Finished!==================="
+
