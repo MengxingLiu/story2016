cbd4aa2a219eaf3b5ff2b950e000b56205fc743f
Max <mengluo1990@gmail.com>
2017-03-14 15:14:17 +0800

    cluster threshold mask

--- s7_clustmask.sh
+++ s7_clustmask.sh
@@ -8,16 +8,16 @@
 ### four paras in parenthesis are Contrast name, index of sub-brick...
 ### threshold of F or T value, and cluster size
 cd /public/home/max/story2016fMRI/group/MVM/ 
-foreach paras("01_Intercept 0 9.284 32" "02_Modality 1 9.284 21" \
+foreach paras("01_Intercept 0 9.284 32" "02_Modality 1 9.284 20" \
 		"03_Scale 2 5.833 31" "04_Mo:Scale 3 5.833 29" \
 		"05_Listen_CS 5 3.047 30" "06_Listen_US 7 3.047 30" \
 		"07_Listen_SW 9 3.047 31" "08_Read_CS 11 3.047 30" \
-		"09_Read_US 13 3.047 31" "10_Read_SW 15 3.047 32" \
+		"09_Read_US 13 3.047 30" "10_Read_SW 15 3.047 32" \
 		"11_Listen 17 3.047 33" "12_Read 19 3.047 33" \
 		"13_Listen_CS-US 21 3.047 13" "14_Listen_CS-SW 23 3.047 25" \
 		"15_Listen_US-SW 25 3.047 22" "16_Read_CS-US 27 3.047 14" \
-		"17_Read_CS-SW 29 3.047 21" "18_Read_US-SW 31 3.047 22" \
-		"19_Read-Listen 33 3.047 15" "20_Listen_Trend 35 2.807 25" "21_Read_Trend 37 2.807 21") 
+		"17_Read_CS-SW 29 3.047 22" "18_Read_US-SW 31 3.047 22" \
+		"19_Read-Listen 33 3.047 15") 
 	set con=(`echo $paras | awk '{print $1}'`)
 	set thindex=(`echo $paras | awk '{print $2}'`)
 	set threshold=(`echo $paras | awk '{print $3}'`)
@@ -32,7 +32,7 @@ foreach paras("01_Intercept 0 9.284 32" "02_Modality 1 9.284 21" \
 		-1Dformat -nosum -1dindex 0 -1tindex 0 \
 		-1thresh $threshold \
 		-dxyz=1 1.01 $clustersize \
-		/public/home/max/story2016fMRI/group/MVM/group_MVM+TREND.nii.gz\["$thindex"]
+		/public/home/max/story2016fMRI/group/MVM/group_MVM.nii\["$thindex"]
 
 end
 	3dbucket *_clustmask.nii.gz -prefix Clustermask.nii.gz
