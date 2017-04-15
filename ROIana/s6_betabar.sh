56e1293aeea22a5991e239c93077b89de95bee45
Max <mengluo1990@gmail.com>
2017-04-15 10:03:04 +0800

    get bar graph

--- ROIana/s6_betabar.sh
+++ ROIana/s6_betabar.sh
@@ -5,7 +5,7 @@
 # ATL 568     Insula 574+576+385   aIFG 377+379    pIFG 597
 # AG 605+547 pMTG 604    SPS 584+587
 # temporal cortex 605 557 604 556
-# AG 547
+# AG 583 605 y > 50
 set spath = /public/home/max/story2016fMRI
 set maskpath =  /public/home/max/story2016fMRI/group/anat_ROIs
 set ROIanapath =  /public/home/max/story2016fMRI/group/ROI_ana
@@ -28,7 +28,7 @@ foreach type("STG 605" "ATL 568" "pMTG 604" "pIFG 597" "AG_2 547")
     end
 end
 
-foreach type("aIFG 377 379" "AG 605 547" "SPS 584 587")
+foreach type("aIFG 377 379" "SPS 584 587")
     set ROI = (`echo $type | awk '{print $1}'`)
     set n = (`echo $type | awk '{print $2}'`)
     set m = (`echo $type | awk '{print $3}'`)
@@ -54,31 +54,22 @@ foreach type("Insula 574 576 385")
     end
 end
 
-# constrain pMTG in 28<y<52  and -9<z<1
-    rm lh_pMTG_small.nii.gz
-    3dcalc -prefix lh_pMTG_small.nii.gz \
-            -a lh_pMTG.nii.gz \
-            -expr 'a*step(144-(y-40)*(y-40))*step(64-(z+7)*(z+7))'
-# constrain STG in  7<y<43
-    rm lh_STG_small.nii.gz
-    3dcalc -prefix lh_STG_small.nii.gz \
-            -a lh_STG.nii.gz \
-            -expr 'a*step(324-(y-25)*(y-25))'
-# select anterior temporal cortex  y<1
-    rm lh_aT.nii.gz
-    3dcalc -prefix lh_aT.nii.gz \
+foreach h(lh rh)
+    rm "$h"_AG.nii.gz
+    3dcalc -a ROIs_"$h".nii.gz \
+        -expr "step(equals(a,583)+equals(a,605))*step(y-50)" \
+        -prefix "$h"_AG.nii.gz
+end
+# select anterior temporal Lobe  y<15
+    rm lh_ATL.nii.gz
+    3dcalc -prefix lh_ATL.nii.gz \
         -a lh_temporal.nii.gz \
-        -expr "a*step(1-y)" 
-# select middle temporal cortex 15<y<24
-    rm lh_mT.nii.gz
-    3dcalc -prefix lh_mT.nii.gz \
+        -expr "a*step(15-y)" 
+# select middle temporal cortex 15<y<24 and -15<z<4
+    rm lh_mTC.nii.gz
+    3dcalc -prefix lh_mTC.nii.gz \
         -a lh_temporal.nii.gz \
-        -expr "a*step(y-15)*step(34-y)"
-# select posterior temporal cortex 49<y<61
-    rm lh_pT.nii.gz 
-    3dcalc -prefix lh_pT.nii.gz \
-        -a lh_temporal.nii.gz \
-        -expr "a*step(y-49)*step(61-y)"
+        -expr "a*step(y-15)*step(34-y)*step(z+15)*step(4-z)"
 foreach task (listen read)
      if ($task == "listen") then
         set sublist = ( sub02 sub03 sub04 sub05 sub06 sub07 sub08 sub09 \
@@ -88,7 +79,7 @@ foreach task (listen read)
                  sub22 sub23 sub24 sub25 sub26 sub27 sub28 \
                  sub29 sub33)
     endif
-    foreach ROI(aIFG pIFG FFG Insula SPS aT mT pT)
+    foreach ROI(aIFG pIFG FFG Insula SPS ATL mTC AG)
         echo "#subj CS US SW" > "$ROIanapath"/"$task"_"$ROI".1D
         foreach sub( $sublist )
             3dmaskave -mask "$maskpath"/lh_"$ROI".nii.gz \
@@ -99,11 +90,10 @@ foreach task (listen read)
         end
     end
 end
-    source /public/software/profile.d/python-3.5.1-gnu.csh
 cd "$ROIanapath"
 foreach task (listen read )
     echo  "KEY CS US SW" > summary_"$task".txt
-    foreach ROI(aIFG pIFG FFG Insula SPS aT mT pT)
+    foreach ROI(aIFG pIFG FFG Insula SPS ATL mTC AG)
         set m1 = (`cat "$task"_"$ROI".1D | tail -16 | dm x2 | stats | grep mean | awk '{printf("%.5f",$3)}'`)
         set m2 = (`cat "$task"_"$ROI".1D | tail -16 | dm x3 | stats | grep mean | awk '{printf("%.5f",$3)}'`)
         set m3 = (`cat "$task"_"$ROI".1D | tail -16 | dm x4 | stats | grep mean | awk '{printf("%.5f",$3)}'`)
@@ -114,26 +104,4 @@ foreach task (listen read )
         echo $ROI "$m1($s1)" "$m2($s2)" "$m3($s3)" >> summary_"$task".txt
     end
 end
-#######################################################################
-## step 7: Plot out the betas values for conditions across two Tasks
-#======================================================================
-foreach task(listen read)
-
-    foreach ROI(aIFG pIFG FFG Insula SPS aT mT pT)
-        printf "KEY\t$task\n" > head.tmp
-        echo "CS US SW" | transpose > key.tmp
-        cat summary_"$task".txt | grep "$ROI" \
-            | dm s2 s3 s4 | transpose > "$task".tmp
-        abut_stat key.tmp "$task".tmp > temp.tmp
-        cat head.tmp temp.tmp | graf -font Arial -fontsize 18 -width 3.6 \
-                            -height 3.6 -linewidth 1 -ymax 0.06 -ymin 0.00 \
-                            -num_major_yticks 3 -num_minor_yticks 1 \
-                            -errbarwidth 0.6 -barborderthickness 1 \
-                            -color0 white \
-        > fig_"$task"_"$ROI".eps
-    end
-end
-
-
-
 
